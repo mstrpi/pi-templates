242a5db9d197e377ebe48a08c56c6edbffa79fd8
MstrPi <121179343+mstrpi@users.noreply.github.com>
2023-12-02 21:54:14 -0500

    Update wgserverclient.sh
    ran through lint in kate. corrected escape double quotes

--- wgserverclient.sh
+++ wgserverclient.sh
@@ -29,17 +29,17 @@ sudo wg
 sleep 3
 echo "What is the name of the 1st peer/device to be added to this VPN setup? "
 read -r client
-echo "Creating config for ${client}. "
+echo "Creating config for \"${client}". \"
 sleep 1
-mkdir "/etc/wireguard/${client}/"
+mkdir /etc/wireguard/"${client}"/
 echo "One moment, please..."
 sleep 2
-wg genkey | tee "/etc/wireguard/${client}/${client}.key" | wg pubkey > "/etc/wireguard/${client}/${client}.pub"
-wg genpsk > "/etc/wireguard/${client}/${client}.psk"
-{ echo "[Peer]" ; echo "PublicKey = $(cat /etc/wireguard/${client}/${client}.pub)" ; echo "PresharedKey = $(cat /etc/wireguard/${client}/${client}.psk)" ; echo "AllowedIPs = 10.100.0.2/32, fd08:4711::2/128" ; echo ; } >> "/etc/wireguard/interfaces/wg0.conf"
+wg genkey | tee /etc/wireguard/"${client}"/"${client}".key | wg pubkey > /etc/wireguard/"${client}"/"${client}".pub
+wg genpsk > /etc/wireguard/"${client}"/"${client}".psk
+{ echo "[Peer]" ; echo "PublicKey = \"$(cat /etc/wireguard/"${client}"/"${client}".pub)\"" ; echo "PresharedKey = \"$(cat /etc/wireguard/"${client}"/"${client}".psk)\"" ; echo "AllowedIPs = 10.100.0.2/32, fd08:4711::2/128" ; echo ; } >> /etc/wireguard/interfaces/wg0.conf
 wg syncconf wg0 <(wg-quick strip wg0)
-{ echo "[Interface]" ; echo "Address = 10.100.0.2/32, fd08:4711::2/128" ; echo "DNS = 10.100.0.1" ; echo "PrivateKey = $(cat /etc/wireguard/${client}.key)" ; echo ; } >> "/etc/wireguard/${client}/${client}.conf"
-echo "Installing QRencode. This will grant the ability to scan a QR code for easily importing this config file to ${client}. "
+{ echo "[Interface]" ; echo "Address = 10.100.0.2/32, fd08:4711::2/128" ; echo "DNS = 10.100.0.1" ; echo "PrivateKey = \"$(cat /etc/wireguard/"${client}".key)"\" ; echo ; } >> /etc/wireguard/"${client}"/"${client}".conf
+echo "Installing QRencode. This will grant the ability to scan a QR code for easily importing this config file to \"${client}\". "
 sleep 3
 if [ "${debian}" = "y" ]
 then sudo apt install -y qrencode
@@ -48,10 +48,10 @@ fi
 echo "Please enter the external IP address, or the domain name for this connection: "
 read -r conn
 echo "Great! Almost there! "
-{ echo "[Peer]" ; echo "AllowedIPs = 10.100.0.1/32, fd08:4711::1/128" ; echo "Endpoint = ${conn}:47111" ; echo "PersistentKeepalive = 25" ; echo "PublicKey = $(cat /etc/wireguard/server/server.pub)" ; echo "PresharedKey = $(cat /etc/wireguard/${client}/${client}.psk)" ; echo ; } >> "/etc/wireguard/${client}/${client}.conf"
+{ echo "[Peer]" ; echo "AllowedIPs = 10.100.0.1/32, fd08:4711::1/128" ; echo "Endpoint = ${conn}:47111" ; echo "PersistentKeepalive = 25" ; echo "PublicKey = \"$(cat /etc/wireguard/server/server.pub)\"" ; echo "PresharedKey = \"$(cat /etc/wireguard/"${client}"/"${client}".psk)\"" ; echo ; } >> /etc/wireguard/"${client}"/"${client}".conf
 echo "Generating QR code: "
 sleep 3
-qrencode -t ansiutf8 < "/etc/wireguard/${client}/${client}.conf"
-echo "Ensure to open port 47111 on your router/firewall and forward requests to this machine's internal IP address. Your VPN should be up and operational after importing the above config (potentially via the generated QR code) to ${client}. "
+qrencode -t ansiutf8 < /etc/wireguard/"${client}"/"${client}".conf
+echo "Ensure to open port 47111 on your router/firewall and forward requests to this machine's internal IP address. Your VPN should be up and operational after importing the above config (potentially via the generated QR code) to \"${client}\". "
 sleep 5
 echo "fin!"
